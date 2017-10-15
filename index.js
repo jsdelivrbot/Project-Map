c611290f8753dcd1608fc2a83107a9167b3b964e
KrisY777 <krisya89@gmail.com>
2017-10-15 11:43:46 -0700

    moved to heroku

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var router = express.Router;
+var request = require("request");
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+  response.render('pages/Main');
+});
+
+
+app.get('/yelpReviews/:id', getYelpReview);
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function getYelpReview(req, res) {
+  var yelpId = req.params["id"];
+  var yelpBaseurl = "https://api.yelp.com/oauth2/token";
+  request.post(yelpBaseurl, {
+    form: {
+      grant_type: "client_credentials",
+      client_id: "iaRX4joOJIg9Da3oMz_raw",
+      client_secret: "ZACyDM68ze0l7H0AWWvkfvVyezfWEFSPIOOCCfgywawXjvKudecJE56SwgKFFb02"
+    }
+  }, function (err, httpResponse, body) {
+    if (err) {
+      res.json({
+        reviews: []
+      });
+    } else {
+      request.get(`https://api.yelp.com/v3/businesses/${yelpId}/reviews`, {
+        json: true,
+        headers: {
+          "Authorization": "Bearer " + JSON.parse(body).access_token
+        }
+      }, function (err, httpResponse, body) {
+        if (err) {
+          res.json({
+            reviews: []
+          });
+        } else {
+          console.log(yelpId);
+          res.json(body);
+        }
+      });
+    }
+  });
+}
